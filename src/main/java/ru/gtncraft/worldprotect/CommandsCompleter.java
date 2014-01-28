afb6114c79f3b822d43ea48a8c0d428e72239829
maximvarentsov <admin@phpcode.us>
2014-01-28 11:23:26 +0400

    Refactoring

--- src/main/java/ru/gtncraft/worldprotect/CommandsCompleter.java
+++ src/main/java/ru/gtncraft/worldprotect/CommandsCompleter.java
@@ -15,40 +15,39 @@ import java.util.*;
 public class CommandsCompleter implements TabCompleter {
 
     private final WorldProtect plugin;
-    private final List<String> ROOT_SUBS = ImmutableList.of(
+    private final List<String> subs = ImmutableList.of(
             "define", "delete", "addowner", "delowner", "addmemeber", "delmember",
             "info", "list", "flag", "save"
     );
-    private final List<String> BOOLEAN = ImmutableList.of("true", "false");
-    private final List<String> FLAGS = new ArrayList<>();
+    private final List<String> bool = ImmutableList.of("true", "false");
+    private final List<String> flags = new ArrayList<>();
 
     public CommandsCompleter(final WorldProtect plugin) {
         this.plugin = plugin;
         for (Flags.prevent flag : Flags.prevent.values()) {
-            FLAGS.add(flag.name());
+            flags.add(flag.name());
         }
     }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        // Remember that we can return null to default to online player name matching
         /**
-         * Remember that we can return null to default to online player name matching
-         *
-         * /<command> define <регион>
-         * /<command> delete <регион>
-         * /<command> addowner <регион> <игрок>
-         * /<command> delowner <регион> <игрок>
-         * /<command> addmember <регион> <игрок>
-         * /<command> delmember <регион> <игрок>
-         * /<command> info [регион]
-         * /<command> list
-         * /<command> flag <регион> set <flag> <true|false>
-         * /<command> save
+          define <region>
+          delete <region>
+          addowner <region> <player>
+          delowner <region> <player>
+          addmember <region> <player>
+          delmember <region> <player>
+          info [region]
+          list
+          flag <region> set <flag> <true|false>
+          save
          */
         String lastArg = args[args.length - 1];
 
         if (args.length <= 1) {
-            return partial(args[0], ROOT_SUBS);
+            return partial(args[0], subs);
         } else if (args.length == 2) {
             String sub = args[0];
             if ("del".equals(sub) || "delete".equals(sub) || "remove".equals(sub) ||
@@ -69,12 +68,12 @@ public class CommandsCompleter implements TabCompleter {
         } else if (args.length == 4) {
             String sub = args[0];
             if ("flag".equals(sub)) {
-                return partial(lastArg, FLAGS);
+                return partial(lastArg, flags);
             }
         } else if (args.length == 5) {
             String sub = args[0];
             if ("flag".equals(sub)) {
-                return partial(lastArg, BOOLEAN);
+                return partial(lastArg, bool);
             }
         }
 
@@ -85,8 +84,8 @@ public class CommandsCompleter implements TabCompleter {
         return StringUtil.copyPartialMatches(token, from, new ArrayList<String>(from.size()));
     }
 
-    private Collection<String> allRegions(Player player) {
-        Collection<String> result = new ArrayList<>();
+    private List<String> allRegions(Player player) {
+        List<String> result = new ArrayList<>();
         if (player.hasPermission(plugin.PERMISSION_ADMIN)) {
             for (Map.Entry<String, Region> entry : plugin.getRegionManager().get(player.getWorld()).entrySet()) {
                 result.add(entry.getKey());
