2c933f11112c15be131fbbe13bef9509d217dbb8
maximvarentsov <admin@phpcode.us>
2014-01-31 14:19:56 +0400

    Add class flag

---
+++ src/main/java/ru/gtncraft/worldprotect/flags/Prevent.java
@@ -0,0 +1,15 @@
+package ru.gtncraft.worldprotect.flags;
+
+public enum Prevent {
+    build,
+    use,
+    damage,
+    teleport,
+    creatureSpawn,
+    command,
+    explode,
+    leavesDecay,
+    grow,
+    fade,
+    burn
+}
