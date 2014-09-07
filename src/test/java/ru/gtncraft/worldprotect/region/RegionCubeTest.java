7386682f9f26c5b311c34822da437052ed2a9ab5
maximvarentsov <admin@phpcode.us>
2014-09-07 15:11:49 +0400

    fix typo

--- src/test/java/ru/gtncraft/worldprotect/region/RegionCubeTest.java
+++ src/test/java/ru/gtncraft/worldprotect/region/RegionCubeTest.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 
-public class RegionCudeTest {
+public class RegionCubeTest {
 
     @Test
     public void serializationTest() {
@@ -17,23 +17,23 @@ public class RegionCudeTest {
         int x2 = 254;
         int y2 = 0;
         int z2 = 254;
-        RegionCube cudeRegion = new RegionCube(x1, y1, z1, x2, y2, z2);
-        cudeRegion.setName("TestRegion");
-        cudeRegion.addFlag(Flag.build);
-        cudeRegion.addFlag(Flag.piston);
-        cudeRegion.addFlag(Flag.pvp);
-        cudeRegion.removeFlag(Flag.piston);
-        cudeRegion.addOwner(UUID.fromString("b8089590-34db-11e4-8c21-0800200c9a66"));
-        cudeRegion.addMember(UUID.fromString("bc13bd90-34db-11e4-8c21-0800200c9a66"));
-        cudeRegion.addMember(UUID.fromString("c7a18110-34db-11e4-8c21-0800200c9a66"));
-        cudeRegion.removeMember(UUID.fromString("c7a18110-34db-11e4-8c21-0800200c9a66"));
+        RegionCube region = new RegionCube(x1, y1, z1, x2, y2, z2);
+        region.setName("TestRegion");
+        region.addFlag(Flag.build);
+        region.addFlag(Flag.piston);
+        region.addFlag(Flag.pvp);
+        region.removeFlag(Flag.piston);
+        region.addOwner(UUID.fromString("b8089590-34db-11e4-8c21-0800200c9a66"));
+        region.addMember(UUID.fromString("bc13bd90-34db-11e4-8c21-0800200c9a66"));
+        region.addMember(UUID.fromString("c7a18110-34db-11e4-8c21-0800200c9a66"));
+        region.removeMember(UUID.fromString("c7a18110-34db-11e4-8c21-0800200c9a66"));
 
-        String json = new Gson().toJson(cudeRegion);
+        String json = new Gson().toJson(region);
 
         String result = "{\"lowerX\":0,\"lowerY\":0,\"lowerZ\":0,\"upperX\":254,\"upperY\":254,\"upperZ\":254,\"name\":\"testregion\",\"owners\":[\"b8089590-34db-11e4-8c21-0800200c9a66\"],\"members\":[\"bc13bd90-34db-11e4-8c21-0800200c9a66\"],\"flags\":[\"build\",\"pvp\"]}\n";
 
-        RegionCube cubeRegion2 = new Gson().fromJson(result, RegionCube.class);
-        String json2 = new Gson().toJson(cubeRegion2);
+        RegionCube region2 = new Gson().fromJson(result, RegionCube.class);
+        String json2 = new Gson().toJson(region2);
 
         assertEquals(json, json2);
     }
