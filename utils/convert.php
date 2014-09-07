a26b95d5846f174e33c26de575598501afe5db6c
maximvarentsov <admin@phpcode.us>
2014-09-07 22:50:46 +0400

    convert from < 0.7 to 1.0

---
+++ utils/convert.php
@@ -0,0 +1,62 @@
+#! /usr/bin/env php
+<?php
+$ts = time();
+foreach (glob("*.json") as $file) {
+    $json = json_decode(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . $file), true);
+
+    $worldFlags = [];
+    $regions = [];
+
+    foreach ($json['world']['flags'] as $key => $value) {
+        if ($value == true) {
+            $worldFlags[] = $key;
+        }
+    }
+    foreach ($json['regions'] as $region) {
+        $lowerX = $region['p1']['x'];
+        $lowerY = $region['p1']['y'];
+        $lowerZ = $region['p1']['z'];
+
+        $upperX = $region['p2']['x'];
+        $upperY = $region['p2']['y'];
+        $upperZ = $region['p2']['z'];
+
+        $name = $region['name'];
+
+        $owners = [];
+        foreach ($region['owners'] as $uuid) {
+            $owners[] = $uuid;
+        }
+
+        $members = [];
+        foreach ($region['members'] as $uuid) {
+            $members[] = $uuid;
+        }
+
+        $flags = [];
+        foreach ($region['flags'] as $key => $value) {
+            if ($value == true) {
+                $flags[] = $key;
+            }
+        }
+
+        $regions[] = [
+            'lowerX' => $lowerX,
+            'lowerY' => $lowerY,
+            'lowerZ' => $lowerZ,
+            'upperX' => $upperX,
+            'upperY' => $upperY,
+            'upperZ' => $upperZ,
+            'createdAt' => $ts++,
+            'name' => $name,
+            'owners' => $owners,
+            'members' => $members,
+            'flags' => $flags
+        ];
+
+        $output = json_encode(['flags' => $worldFlags, 'regions' => $regions], JSON_PRETTY_PRINT);
+        file_put_contents($file . ".converted", $output);
+    }
+}
+
+
