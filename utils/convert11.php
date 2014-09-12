c387f1d93ba58a8d371aeb39e6123182e69f0779
maximvarentsov <admin@phpcode.us>
2014-09-12 19:13:04 +0400

    convert from 1.0 to 1.1

---
+++ utils/convert11.php
@@ -0,0 +1,34 @@
+#! /usr/bin/env php
+<?php
+foreach (glob("*.json") as $file) {
+    $json = json_decode(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . $file), true);
+
+    $json['flags'] = fixFlags($json['flags']);
+
+    foreach ($json['regions'] as &$region) {
+        $region['flags'] = fixFlags($region['flags']);
+    }
+
+    $output = json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    file_put_contents($file . ".converted", $output);
+}
+
+function fixFlags(array $flags) {
+    $result = [];
+    foreach ($flags as $flag) {
+        if ($flag == 'fireSpread' || $flag == 'fallingBlocks') {
+            continue;
+        }
+        if ($flag == 'leavesDecay') {
+            $flag = 'grow';
+        }
+        if ($flag == 'fade') {
+            $flag = "spread";
+        }
+        if ($flag == 'explode') {
+            $flag = "entityBlockExplode";
+        }
+        $result[] = $flag;
+    }
+    return array_unique($result);
+}
